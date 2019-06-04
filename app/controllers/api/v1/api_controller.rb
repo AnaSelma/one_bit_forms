a2fe67d7e15e2ce3e3e3257ff4bb249667550221
Selma <anaselma1962@gmail.com>
2019-06-04 14:52:21 -0300

    Adjusts in Genfile, initializers, rack_attack.rb

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,7 +1,8 @@
-module Api::V1
-  class ApiController < AplicationController
-    # Métodos Globais
+module Api
+  module V1
+    class ApiController < AplicationController
+      # Métodos Globais
 
+    end
   end
-
 end
